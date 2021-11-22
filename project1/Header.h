2b04c476a9de1431beec527ae0096106c032d1b7
elena liarou <79643247+elenaliar@users.noreply.github.com>
2021-11-22 19:38:37 +0200

    Create Header.h

---
+++ project1/Header.h
@@ -0,0 +1,12 @@
+//
+// filename: Header.h
+//  askisi4 protis ergasias 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Item.h"
+#include "STACK.h"
+
+int stringlength(char *str);
+int checkPalindrome(char *str);
